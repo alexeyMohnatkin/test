48bef30b8ed01b19f35525b421a098ac92d2c335
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2017-07-25 08:59:35 +0300

    fix document title

--- server/template.js
+++ server/template.js
@@ -4,7 +4,7 @@ export default function({ spriteContent, application, finalState, css, js }) {
 		<head>
 			<meta charset="utf-8">
 			<meta http-equiv="x-ua-compatible" content="ie=edge">
-			<title>Shakakode admin</title>
+			<title>Shakacode admin</title>
 			<meta name="description" content="">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link rel="stylesheet" href="${css}">
